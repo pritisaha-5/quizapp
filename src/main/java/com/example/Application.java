40d70b8a1018e74d21b3aee1161bdccf5eaabdf9
pritisaha-5 <sahapriti1115@gmail.com>
2025-12-12 23:11:16 +0530

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,15 +2,10 @@ package com.example;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@Component
 @Theme("default")
 public class Application implements AppShellConfigurator {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 
 }
